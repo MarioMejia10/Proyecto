6d6fb09f5befb6649b3b1025c5228e40f2d54edf
MarioMejia10 <87398966+MarioMejia10@users.noreply.github.com>
2021-07-14 19:19:54 -0600

    Operaciones_Matematicas

---
+++ Ejercicio3.java
@@ -0,0 +1,32 @@
+package Ejercicios_Practicas;
+import javax.swing.*;
+/**
+ *Mario Mejia
+ */
+public class Ejercicio3 {
+ 
+public static void main (String [] args){
+
+int M=6, T=1, K=-10;
+
+if (M > T){
+System.out.println("La evaluacion es verdadera");
+}
+else{
+System.out.println("La evalucion es falsa");
+}
+if (T / K == -5){
+System.out.println("La evaluacion es verdadera");
+}
+else{
+System.out.println("La evalucion es falsa");
+}
+if (M + T== 7 && M - T== 5){
+System.out.println("La evaluacion es verdadera");
+}
+else{
+System.out.println("La evalucion es falsa");
+}
+}
+}
+
