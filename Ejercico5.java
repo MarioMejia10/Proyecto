e409cc355b5ef76a66ef1a42f6b124b81225f0c5
MarioMejia10 <87398966+MarioMejia10@users.noreply.github.com>
2021-07-14 18:19:42 -0600

    Operaciones_Matematicas

---
+++ Ejercico5.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+public class Ejercicio5
+{
+    public static void main (String args[]){
+     int numI=1;
+     int numF=100;
+     
+     while(numI <= 100){
+     if(numI%2==0){
+  
+     System.out.println(numI);
+  }
+  numI++;
+ }
+    }
+}
